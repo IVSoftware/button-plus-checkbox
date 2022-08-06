c9d219536a9b4276e557d7051eca7bce68ed6441
Tom Gregor <tgregor@ivsoftware.com>
2022-08-06 08:09:49 -0600

    Renamed Value=>Moneycount

--- button_plus_checkbox/MainForm.cs
+++ button_plus_checkbox/MainForm.cs
@@ -13,35 +13,32 @@ namespace button_plus_checkbox
             numericUpDown.DataBindings.Add(
                 nameof(NumericUpDown.Value), 
                 this, 
-                nameof(Value),
+                nameof(Moneycount),
                 false,
                 DataSourceUpdateMode.OnPropertyChanged);
         }
-
         private void mButton_Click(object sender, EventArgs e)
         {
-            Value += level1.Checked ? 2 : 1;
+            Moneycount += level1.Checked ? 2 : 1;
         }
-
         private void level1_CheckedChanged(object sender, EventArgs e)
         {
             // Clicking up/down arrows increments 1 or 2
             numericUpDown.Increment = level1.Checked ? 2 : 1;
         }
-
-        int _Value = 0;
-        public int Value
+        public int Moneycount
         {
-            get => _Value;
+            get => _moneyCount;
             set
             {
-                if(!Equals(_Value, value))
+                if(!Equals(_moneyCount, value))
                 {
-                    _Value = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+                    _moneyCount = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Moneycount)));
                 }
             }
         }
+        int _moneyCount = 0;
         public event PropertyChangedEventHandler PropertyChanged;
     }
 }
